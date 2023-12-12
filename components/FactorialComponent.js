ccb4c81fe3a12b0f2e3036130e152363c2c4a6ae
kumawat-Ankita <145592877+kumawat-Ankita@users.noreply.github.com>
2023-12-12 13:52:45 +0530

    Update FactorialComponent.js

--- components/FactorialComponent.js
+++ components/FactorialComponent.js
@@ -3,14 +3,8 @@ import React, { useMemo } from 'react';
 const FactorialComponent = ({ number }) => {
   const factorial = useMemo(() => {
     const calculateFactorial = (n) => {
-      if (n === 0 || n === 1) {
-        return 1;
-      }
-      let result = 1;
-      for (let i = 2; i <= n; i++) {
-        result *= i;
-      }
-      return result;
+      if (n <= 1) return 1;
+      return n * calculateFactorial(n - 1);
     };
 
     return calculateFactorial(number);
