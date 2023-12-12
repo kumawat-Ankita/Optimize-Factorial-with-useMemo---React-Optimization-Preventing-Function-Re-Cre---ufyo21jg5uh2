76ba2be6de14c884ef48f7790d45ee68cd98a23e
kumawat-Ankita <145592877+kumawat-Ankita@users.noreply.github.com>
2023-12-12 13:46:09 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,25 @@
-"use client"
 import { useState } from 'react';
 import FactorialComponent from '../components/FactorialComponent';
+
 export default function Home() {
-  
-     
+  const [inputNumber, setInputNumber] = useState(0);
+
+  const handleInputChange = (e) => {
+    setInputNumber(parseInt(e.target.value, 10));
+  };
+
   return (
     <div>
-        <h1>Welcome to Next.js!</h1>
-        <section>
-                <h2>Factorial Calculation</h2>
-                <input 
-                    type="number" 
-                    
-                   } 
-                />
-       
-                <FactorialComponent  />
-            </section>
+      <h1>Welcome to Next.js!</h1>
+      <section>
+        <h2>Factorial Calculation</h2>
+        <input
+          type="number"
+          value={inputNumber}
+          onChange={handleInputChange}
+        />
+        <FactorialComponent number={inputNumber} />
+      </section>
     </div>
-);
+  );
 }
